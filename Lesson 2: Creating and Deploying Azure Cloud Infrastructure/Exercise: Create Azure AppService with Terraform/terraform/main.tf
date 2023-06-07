9eb13d5682a3f6b590b586c8fd32a95933c35b20
brem02 <122722304+brem02@users.noreply.github.com>
2023-06-07 23:00:01 +0200

    Update main.tf

--- Lesson 2: Creating and Deploying Azure Cloud Infrastructure/Exercise: Create Azure AppService with Terraform/terraform/main.tf
+++ Lesson 2: Creating and Deploying Azure Cloud Infrastructure/Exercise: Create Azure AppService with Terraform/terraform/main.tf
@@ -14,7 +14,8 @@ terraform {
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
+  #source               = "../../modules/resource_group"
+  source               = "/modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
